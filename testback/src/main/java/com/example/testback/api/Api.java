24af3a2c980a5d950a7902d9465749a7a7e8d39d
unknown <omarbgm4@gmail.com>
2022-06-01 10:34:13 +0100

    new

--- testback/src/main/java/com/example/testback/api/Api.java
+++ testback/src/main/java/com/example/testback/api/Api.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Api {
 
-    @GetMapping("/")
+    @GetMapping("/hello")
     public String restAPI() {
         return " hello world ";
     }
